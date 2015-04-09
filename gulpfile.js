c6365071fd94425ca0341eae2dbfa3fd1afd41dc
Gregory Hilkert <EpiphanyMachine@gmail.com>
2015-04-08 20:53:36 -0700

    dont package dependencies, revert jquery upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
+/* globals require */
+
+'use strict';
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 var paths = {
-  vendor: [
-       'bower_components/lodash/dist/lodash.min.js',
-       'bower_components/jQuery/dist/jquery.min.js'
-      ],
   src: 'src/behave.js'
 };
 
 gulp.task('default', function() {
-  var build_files = paths.vendor.concat(paths.src)
-  return gulp.src(build_files)
+  return gulp.src(paths.src)
     .pipe(concat('behave.js'))
     .pipe(gulp.dest('./dist/'))
     .pipe(concat('behave.min.js'))
